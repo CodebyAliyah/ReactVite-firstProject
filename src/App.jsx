959847ff4664b7e2753bdc70486999961b451048
Ali Yahya <165564206+CodebyAliyah@users.noreply.github.com>
2024-09-08 21:19:57 -0700

    Code Done

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import iTem from "./Hello" 
+function App() {
+  const userName = 'Ali' 
+  return (
+     
+    <>
+      <h1>Hello, {userName} Welcome to React-Series With Sir Hitesh-Choudhary</h1>
+    </>
+  )
+}
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,10 @@
+import 
+function iTem 
+return (
+    <>
+    <h1>Hello, Sir Hitesh you are best Coder</h1>
+    </>
+  )
+}
+
+export default iTem
